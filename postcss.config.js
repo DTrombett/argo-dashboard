6876b15ecca2f2e04dc6f7f71610de92e20cb056
DTrombett <maxtromb.dt@gmail.com>
2023-11-10 16:22:31 +0100

    feat: start project

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+	plugins: {
+		"postcss-flexbugs-fixes": {},
+		"postcss-preset-env": {
+			autoprefixer: {
+				flexbox: "no-2009",
+			},
+			stage: 3,
+			features: {
+				"custom-properties": false,
+			},
+		},
+		"@fullhuman/postcss-purgecss": {
+			content: ["./**/*.{js,jsx,ts,tsx}"],
+			defaultExtractor: (/** @type {string} */ content) =>
+				content.match(/[\w-/:]+(?<!:)/g) || [],
+			safelist: ["html", "body"],
+		},
+		"tailwindcss": {},
+		"autoprefixer": {},
+	},
+};
