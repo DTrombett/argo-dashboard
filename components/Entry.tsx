421bd1d455c8d4b68ecd3b4381a2bc38db2e5801
DTrombett <maxtromb.dt@gmail.com>
2023-11-24 20:34:06 +0100

    fix: padding too high

--- components/Entry.tsx
+++ components/Entry.tsx
@@ -9,7 +9,7 @@ const Entry = ({
 	name: string;
 	children?: React.ReactNode;
 }) => (
-	<div className="flex flex-col max-w-full border-t-2 max-h-64 p-4 lg:h-64">
+	<div className="flex flex-col max-w-full border-t-2 max-h-64 py-4 lg:h-64 lg:px-2">
 		<span className={semiBold.className}>{name}</span>
 		<div className="flex flex-col mt-2 px-2 max-h-64 h-full overflow-y-auto">
 			{children}
