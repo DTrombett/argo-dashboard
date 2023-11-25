372b4c431fbc54f2950505bc2cb4ed24c5084fe0
DTrombett <maxtromb.dt@gmail.com>
2023-11-25 14:36:23 +0100

    chore: add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export const metadata: Metadata = {
 	creator: "D Trombett",
 	generator: "Next.js",
 	publisher: "Vercel",
+	other: {
+		"google-site-verification": "TAmOsVi35BnusH1-Lx2BJLhd3O42orZcSRdGt2QON6A",
+	},
 };
 
 export default function RootLayout({
