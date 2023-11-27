b823440dc37613039d35cabe253e5b4665c24d6a
DTrombett <maxtromb.dt@gmail.com>
2023-11-27 20:23:21 +0100

    fix: login screen incorrect width

--- components/PasswordField.tsx
+++ components/PasswordField.tsx
@@ -7,10 +7,10 @@ const PasswordField = () => {
 	const [show, setShow] = useState(false);
 
 	return (
-		<div>
+		<div className="m-2 max-w-full">
 			<input
 				autoComplete="current-password"
-				className="input"
+				className="p-4 max-w-full rounded outline-none bg-zinc-200 dark:bg-zinc-800 duration-200 text-xl hover:bg-opacity-80 sm:text-2xl"
 				name="password"
 				placeholder="Password"
 				type={show ? "text" : "password"}
