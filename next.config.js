a1183272e7b5ac782bdf13d52a9a3a5c4438a762
DTrombett <maxtromb.dt@gmail.com>
2024-01-11 16:43:53 +0100

    feat: update csp

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'self';
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	webpack: (config, { isServer }) => {
@@ -38,12 +52,11 @@ const nextConfig = {
 	},
 	headers: async () => [
 		{
-			source: "/:path*",
+			source: "/(.*)",
 			headers: [
-				{ key: "Content-Security-Policy", value: "frame-ancestors 'self'" },
+				{ key: "Content-Security-Policy", value: cspHeader.replace(/\n/g, "") },
 				{ key: "X-Frame-Options", value: "SAMEORIGIN" },
 				{ key: "X-Content-Type-Options", value: "nosniff" },
-				{ key: "X-XSS-Protection", value: "0" },
 			],
 		},
 	],
