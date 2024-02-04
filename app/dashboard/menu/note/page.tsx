503994643010c4718aa78e9ce67e3b85eb25a701
DTrombett <maxtromb.dt@gmail.com>
2024-02-04 11:55:00 +0100

    fix: clipboard not always available

--- app/dashboard/menu/note/page.tsx
+++ app/dashboard/menu/note/page.tsx
@@ -14,15 +14,18 @@ const Note = () => {
 	return (
 		<>
 			{dashboard?.noteDisciplinari.length ? (
-				<code className="m-8 p-4 whitespace-break-spaces break-all text-left bg-zinc-200 dark:bg-zinc-800 rounded-xl relative">
-					<FontAwesomeIcon
-						icon={faCopy}
-						className="absolute right-4 top-4 h-8 p-3 rounded-lg cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-700 hover:bg-opacity-80 dark:hover:bg-opacity-80"
-						onClick={navigator.clipboard.writeText.bind(
-							navigator.clipboard,
-							data
-						)}
-					/>
+				<code className="m-2 sm:m-4 md:m-6 lg:m-8 p-4 whitespace-break-spaces break-all text-left bg-zinc-200 dark:bg-zinc-800 rounded-xl relative">
+					{/* eslint-disable-next-line @typescript-eslint/no-unnecessary-condition */}
+					{navigator.clipboard && (
+						<FontAwesomeIcon
+							icon={faCopy}
+							className="absolute right-4 top-4 h-8 p-3 rounded-lg cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-700 hover:bg-opacity-80 dark:hover:bg-opacity-80"
+							onClick={navigator.clipboard.writeText.bind(
+								navigator.clipboard,
+								data
+							)}
+						/>
+					)}
 					{data}
 				</code>
 			) : (
